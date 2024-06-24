e72be2138d78ee4187156395524b716372e1ffe5
AlfaridzaHamdani <alfaridzah@gmail.com>
2024-06-24 23:48:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: "#ab3434",
+      secondary: "#491313",
+      black: "#000",
+      white: "#fff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
